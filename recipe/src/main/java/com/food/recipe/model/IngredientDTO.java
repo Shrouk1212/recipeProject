dd87680faf6194c194e138e95a3dfc5dc7edef8d
shrouk <shrouq.farrag@gmail.com>
2022-11-14 14:23:47 +0200

    cleaning the code

--- recipe/src/main/java/com/food/recipe/model/IngredientDTO.java
+++ recipe/src/main/java/com/food/recipe/model/IngredientDTO.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.food.recipe.entity.RecipeEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
